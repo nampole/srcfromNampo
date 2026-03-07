7b042864fec9d52f619086ddafcff2bee50021da
nampole <nampole@aol.com>
2026-03-07 19:00:31 +0900

    D1

--- pythonCode/somebooksrccode_py/CircleSpiral1.py
+++ pythonCode/somebooksrccode_py/CircleSpiral1.py
@@ -5,6 +5,7 @@ turtle.bgcolor("black")
 # You can choose between 2 and 6 sides for some cool shapes!
 sides = 6
 colors = ["red", "yellow", "blue", "orange", "green", "purple"]
+print(type(colors))
 for x in range(360):
     t.pencolor(colors[x%sides])
     t.forward(x * 3/sides + x)
