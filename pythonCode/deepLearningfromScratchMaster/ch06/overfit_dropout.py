c0e90aaba5f00d662c22ca074cf44d264d8c008e
nampole <nampole@aol.com>
2026-03-13 22:26:00 +0900

    ASDF

--- pythonCode/deepLearningfromScratchMaster/ch06/overfit_dropout.py
+++ pythonCode/deepLearningfromScratchMaster/ch06/overfit_dropout.py
@@ -8,7 +8,7 @@ import sys
 sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
 import matplotlib.pyplot as plt
-from dataset.mnist import load_mnist
+from ..dataset.mnist import load_mnist
 from common.multi_layer_net_extend import MultiLayerNetExtend
 from common.trainer import Trainer
 
