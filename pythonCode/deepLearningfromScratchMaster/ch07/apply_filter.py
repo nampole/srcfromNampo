f684b601700f383aefcb8a6292641a0bf680f0ac
nampole <nampole@aol.com>
2026-03-26 15:02:24 +0900

    long time

--- pythonCode/deepLearningfromScratchMaster/ch07/apply_filter.py
+++ pythonCode/deepLearningfromScratchMaster/ch07/apply_filter.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 import sys, os
 sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..') 
+
 import numpy as np
 import matplotlib.pyplot as plt
 from simple_convnet import SimpleConvNet
