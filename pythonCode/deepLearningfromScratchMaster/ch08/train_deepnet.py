3cc11f14772227492acfa507dccd3d22d20a3b97
nampole <nampole@aol.com>
2026-05-22 22:44:24 +0900

    Fix formatting in train_deepnet.py for consistency

--- pythonCode/deepLearningfromScratchMaster/ch08/train_deepnet.py
+++ pythonCode/deepLearningfromScratchMaster/ch08/train_deepnet.py
@@ -7,9 +7,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
 from deep_convnet import DeepConvNet
-from common.trainer import Trainer
+from common.trainer import Trainer 
 
-(x_train, t_train), (x_test, t_test) = load_mnist(flatten=False)
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=False) 
 
 network = DeepConvNet()  
 trainer = Trainer(network, x_train, t_train, x_test, t_test,
